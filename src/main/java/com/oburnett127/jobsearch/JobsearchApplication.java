23b6c08fc02405a511bfa60d8083e01fb6e65978
Owen Burnett <oburnett127@gmail.com>
2023-03-27 20:35:24 -0500

    Added dependencies to pom.xml for JWT.

--- src/main/java/com/oburnett127/jobsearch/JobsearchApplication.java
+++ src/main/java/com/oburnett127/jobsearch/JobsearchApplication.java
@@ -2,14 +2,7 @@ package com.oburnett127.jobsearch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableJpaRepositories

d596286978c1d70892a57fb774b7221d4b0d9e34
Owen Burnett <oburnett127@gmail.com>
2023-07-14 01:34:44 -0500

    Changed JobUpdateRequest.java, chgd from employer to employerId

--- src/main/java/com/oburnett127/jobsearch/model/request/JobUpdateRequest.java
+++ src/main/java/com/oburnett127/jobsearch/model/request/JobUpdateRequest.java
@@ -1,14 +1,12 @@
 package com.oburnett127.jobsearch.model.request;
 
-import com.oburnett127.jobsearch.model.Employer;
-
 import lombok.Data;
 
 @Data
 public class JobUpdateRequest {
     private int id;
     private String title;
-    private Employer employer;
+    private int employerId;
     private String description;
     private String postDate;
 }
